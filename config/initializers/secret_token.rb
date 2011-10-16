5558b28b60af8d84fdc66a063ea9b3e5c6c82349
Nathan <nathan_stults@hsihealth.com>
2011-10-16 16:23:55 -0700

    dem bones

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hereiam::Application.config.secret_token = '8b7ca2ce60737a6a1c1156431a3fb35be2823d8166f9398623cfbedc25a614407676846c8c90ea722e12f28ba38f0c783fc62fda8f1e1a86ea538a5861a0fe2f'
